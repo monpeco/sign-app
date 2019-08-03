494b255871fbbfeca26ee727c662b512bf9eca27
Alejandro Montilla <monpeco@gmail.com>
2019-08-03 23:08:02 +0200

    Add root home, about and contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'store#home'
+  get 'store/home'
+  get 'store/about'
+  get 'store/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
