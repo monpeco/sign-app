22257f2ca30562fe134a102f72cf05e1690ac4d8
Alejandro Montilla <monpeco@gmail.com>
2019-08-06 23:11:31 +0200

    Add helper method currect_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id]).email
+    else
+      @current_user = nil
+    end
+  end
+
 end
