0f4cc395c395de47d89070488f8ea912b0c16a61
Alejandro Montilla <monpeco@gmail.com>
2019-08-16 00:30:05 +0200

    Run db:migrate on Property

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_202737) do
+ActiveRecord::Schema.define(version: 2019_08_15_222839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,15 @@ ActiveRecord::Schema.define(version: 2019_08_15_202737) do
     t.index ["name"], name: "index_products_on_name", unique: true
   end
 
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.string "value"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_properties_on_product_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -87,4 +96,5 @@ ActiveRecord::Schema.define(version: 2019_08_15_202737) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "attributes", "products"
   add_foreign_key "products", "categories"
+  add_foreign_key "properties", "products"
 end
