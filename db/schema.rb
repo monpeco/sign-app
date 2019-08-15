9cd4e48a9bdd60f218ac1d068849d2a0ea2bd5d5
Alejandro Montilla <monpeco@gmail.com>
2019-08-15 22:29:03 +0200

    Run db:migrate on Attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_202141) do
+ActiveRecord::Schema.define(version: 2019_08_15_202737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2019_08_15_202141) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "attributes", force: :cascade do |t|
+    t.string "name"
+    t.string "value"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_attributes_on_product_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -76,5 +85,6 @@ ActiveRecord::Schema.define(version: 2019_08_15_202141) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "attributes", "products"
   add_foreign_key "products", "categories"
 end
