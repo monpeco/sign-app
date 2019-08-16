5004f6ef0942e0292b9bc1be8456bb2d3f2555e5
Alejandro Montilla <monpeco@gmail.com>
2019-08-16 20:50:18 +0200

    Add multiple pictures to Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:code, :name, :price, :description, :date_added, :active, :deleted, :comment, :promo, :category_id)
+      params.require(:product).permit(:code, :name, :price, :description, :date_added, :active, :deleted, :comment, :promo, :category_id, images: [])
     end
 end
