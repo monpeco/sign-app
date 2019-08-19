dd68acec12d45188e4574589b9e714385bb83d2f
Julio Lopez <julioj.lopeza@gmail.com>
2019-08-18 21:21:04 -0400

    images carousel with bulma-carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,4 +24,31 @@ $(document).ready(function() {
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
     });
+    
+    // Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('#carousel-demo', {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        infinite: true,
+        duration: 500,
+        autoplay: true,
+        autoplaySpeed: 5000,
+
+    });
+
+    // Loop on each carousel initialized
+    for(var i = 0; i < carousels.length; i++) {
+        // Add listener to  event
+        carousels[i].on('before:show', state => {
+            console.log(state);
+        });
+    }
+    // Access to bulmaCarousel instance of an element
+    var element = document.querySelector('#my-element');
+    if (element && element.bulmaCarousel) {
+        // bulmaCarousel instance is available as element.bulmaCarousel
+        element.bulmaCarousel.on('before-show', function(state) {
+            console.log(state);
+        });
+    }
 });
