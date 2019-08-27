49359562f847e0472b6a0dd2a92f35a3882df75b
Julio Lopez <julioj.lopeza@gmail.com>
2019-08-26 22:51:30 -0400

    all the routes to react-component index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :categories
-  root to: 'store#home'
   
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  get 'sessions', to: 'sessions#new'
-  get 'store/home'
-  get 'store/about'
-  get 'store/contact'
+  ## resources :products
+  ## resources :categories
+  root to: 'store#home'
+  get '*path', to: 'store#home'
+  ## resources :users
+  ## resources :sessions, only: [:new, :create, :destroy]
+  ## get 'sessions', to: 'sessions#new'
+  ## get 'store/home'
+  ## get 'store/about'
+  ## get 'store/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
