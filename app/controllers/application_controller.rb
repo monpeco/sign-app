0703a3d8f7623b8274e8a0903c1a1d7b0bbab652
Julio Lopez <julioj.lopeza@gmail.com>
2019-08-29 00:37:31 -0400

    skiping normal behavior of rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
+  skip_before_action verify_authenticity_token
 
-  helper_method :current_user
-
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id]).email
-    else
-      @current_user = nil
-    end
-  end
+  # helper_method :current_user
+  # 
+  # def current_user
+  #   if session[:user_id]
+  #     @current_user ||= User.find(session[:user_id]).email
+  #   else
+  #     @current_user = nil
+  #   end
+  # end
 
 end
