75551dd91f4faf36db5f24ee624872d603e084d6
Julio Lopez <julioj.lopeza@gmail.com>
2019-08-29 00:36:35 -0400

    sessions controller modified to work as an api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,38 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
+  
   def new
-    @is_log_in = true
+    if @current_user
+      render json: {
+        user: @current_user
+      }
+    end
   end
 
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      render json: {
+        status: :created,
+        user: user
+      }
       # redirect_to root_url, notice: 'Log in!'
-      flash.now[:alert] = 'USUARIO LOGEADO'
-      redirect_to root_url
+      # flash.now[:alert] = 'USUARIO LOGEADO'
+      # redirect_to root_url
     else
-      flash.now[:alert] = 'Email o password incorrecto'
-      render 'new'
+      render json: {
+        status: 401
+      }
+      # flash.now[:alert] = 'Email o password incorrecto'
+      # render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: 'Log out!:'
+    render json: { status: 200 }
+    
+    # redirect_to root_url, notice: 'Log out!:'
   end
 end
