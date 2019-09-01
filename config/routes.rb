7a8ff30b8c6b50b850e5b5353085d942a82855d9
CyDIngenieria <jjlopez@cydingenieria.cl>
2019-09-01 10:38:00 -0400

    new routes to check if the user is logged in and log out route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  
   ## resources :products
   ## resources :categories
   root to: 'store#home'
-  get '*path', to: 'store#home'
   ## resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :registration, only: [:create]
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  get :logged_in , to: 'sessions#logged_in'
+  delete :log_out , to: 'sessions#log_out'
   ## get 'sessions', to: 'sessions#new'
   ## get 'store/home'
   ## get 'store/about'
