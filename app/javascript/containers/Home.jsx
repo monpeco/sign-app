139ce0da198da688aa4a337c9d20d2c10c2dc6fd
CyDIngenieria <jjlopez@cydingenieria.cl>
2019-09-01 10:35:18 -0400

    spinner and home views

---
+++ app/javascript/containers/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from '../components/Carousel'
+
+export default class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <Carousel />
+            </div>
+        )
+    }
+}
