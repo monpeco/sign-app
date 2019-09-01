1983232d891bc783d546bea696f0b2034711b154
CyDIngenieria <jjlopez@cydingenieria.cl>
2019-09-01 10:37:19 -0400

    App wrapper component checking if the user is logged in

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -1,17 +1,47 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
 import { Route } from 'react-router-dom'
-import Navegacion from '../components/Navegacion'
-import Carousel from '../components/Carousel'
+import Home from '../containers/Home'
+import Navbar from '../components/Navbar'
+import Login from '../containers/Login'
+import SignUp from '../containers/SignUp'
+import * as userDuck from '../ducks/User'
 
 class App extends React.Component{
+    
+    componentDidMount(){
+        const { checkUser } = this.props
+        checkUser()
+    }
+
+    handleSignOut = e => {
+        const { signOut } = this.props
+        signOut()
+    }
+
     render(){
+        const { user } = this.props
+        console.log(user)
         return(
             <div>
-                <Route path="/" component={Navegacion} />
-                <Route exact={true} path="/" component={Carousel} />
+                <Route path="/" render={() => <Navbar user={user} handleSignOut={this.handleSignOut} />} />
+                <Route exact={true} path="/" component={Home} />
+                <Route exact={true} path="/login" component={Login} />
+                <Route exact={true} path="/signup" component={SignUp} />
             </div>
         )
     }
 }
 
-export default App
+const mapStateToProps = state => {
+    return {
+        user: state.User
+    }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+    ...userDuck,
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
