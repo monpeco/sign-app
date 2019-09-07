6a639e3d402bd1ed72bcdd65ac41a34cf536dc2d
Alejandro Montilla <monpeco@gmail.com>
2019-09-07 11:31:54 +0200

    Add spec to check to user create to check presence of email

--- spec/features/create_user_feature.rb
+++ spec/features/create_user_feature.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
  
 RSpec.describe 'Creating a user', type: :feature do
+  
   scenario 'valid inputs' do
     visit new_user_path
     fill_in 'user_email', with: 'sample@example.com'
@@ -10,4 +11,14 @@ RSpec.describe 'Creating a user', type: :feature do
     visit users_path
     expect(page).to have_content('sample@example.com')
   end
+
+  scenario 'invalid inputs - email missing' do
+    visit new_user_path
+    fill_in 'user_email', with: ''
+    fill_in 'user_password', with: 'sample'
+    fill_in 'user_password_confirmation', with: 'sample'
+    click_on 'Crear Usuario'
+    expect(page).to have_content('error')
+  end
+
 end
