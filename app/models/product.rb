de21cb2271181fd1ab935a4a50b39bee8ba35917
Alejandro Montilla <monpeco@gmail.com>
2019-09-10 22:35:29 +0200

    Add commment of fields to product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
+# ["id", "code", "name", "price", "description", "date_added", "active", "deleted", "comment", "promo", "category_id", "created_at", "updated_at"] 
+
 class Product < ApplicationRecord
   belongs_to :category
   has_many :properties
