b729b64826156a3d617c7672d139101ed318c86b
CyDIngenieria <jjlopez@cydingenieria.cl>
2019-09-15 23:47:59 -0300

    managing session log in

--- app/javascript/containers/Login.jsx
+++ app/javascript/containers/Login.jsx
@@ -3,24 +3,54 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
 import LoginForm from '../components/LoginForm'
 import Box from '../components/Box'
+import Modal from '../components/Modal'
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import * as userDuck from '../ducks/User'
+import * as messageDuck from '../ducks/Messages'
 
 class Login extends React.Component{
-
-    handleLogin = async values => {
-        const { LogInUser, history } = this.props
-        await LogInUser(values)
-        history.push("/")
+    
+    state={
+        open: false,
+        action: false,
+        confirmation: false,
     }
 
+    handleClose = e => {
+        this.setState({ ...this.state, open: false })
+    } 
+
+    handleLogin = async values => {
+        const { LogInUser, history, assignMessage, user } = this.props
+        console.log(user)
+        await LogInUser(values)
+        console.log(user)
+        if (user.error){
+            assignMessage("Usuario o contraseña incorrecta!, intente nuevamente")
+            this.setState({ ...this.state, open: true, confirmation: false, action: false })
+        }else{
+            history.push("/")
+        }
+
+    }
+
+
     render(){
-        const { user } = this.props
+        const { user, message } = this.props
         return(
             <MDBContainer className="my-4">
                 <Box>
                     <LoginForm user={user} onSubmit={this.handleLogin}/>
                 </Box>
+                <Modal
+                    open={this.state.open}
+                    handleClose={this.handleClose}
+                    action={this.state.action}
+                    loading={user.fetching}
+                    confirmation={this.state.confirmation}
+                >
+                    {message.data}
+                </Modal>
             </MDBContainer>
         )
     }
@@ -30,12 +60,14 @@ class Login extends React.Component{
 const mapStateToProps = state => {
     console.log(state)
     return {
-        user: state.User
+        user: state.User,
+        message: state.Messages
     }
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({
     ...userDuck,
+    ...messageDuck,
 }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login)
